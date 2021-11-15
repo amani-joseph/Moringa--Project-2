0f38a5c6dc62754605bb375b03c56164b24ca1e0
Joseph Amani <mr.amanijoseph@gmail.com>
2021-11-16 02:13:56 +0300

    Final project ready for hosting

--- JS/main.js
+++ JS/main.js
@@ -1,16 +1,47 @@
-// # SMOOTH SCROLL
-window.addEventListener('scroll',() => {
-    const scrolled = window.scrollY;
-    console.log("scrolled");
-} );
+// PRELOADER
+setTimeout(function(){
+    $('#preloader').fadeToggle();
+}, 1500);
 
-// # SMOOTH SCROLL END
+// WELCOME ANIMATION
+var textWrapper = document.querySelector('.ml10 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-//!BORDER FOR PRODUCT CARDS
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml10 .letter',
+    rotateY: [-90, 0],
+    duration: 1300,
+    delay: (el, i) => 45 * i
+  }).add({
+    targets: '.ml10',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
+ // TO DELANI STUDIOS ANIMATION
+var textWrapper = document.querySelector('.ml3');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml3 .letter',
+    opacity: [0,1],
+    easing: "easeInOutQuad",
+    duration: 1250,
+    delay: (el, i) => 150 * (i+1)
+  }).add({
+    targets: '.ml3',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
 
 
 
-//!BORDER FOR PRODUCT CARDS END
 
 
 
@@ -19,17 +50,5 @@ window.addEventListener('scroll',() => {
 
 
 
-//* SCROLL TO TOP START
-const scrollBtn = document.querySelector(".scroll-to-top");
 
 
-scrollBtn.addEventListener('click', () => {
-    document.documentElement.scrollTop = 0;
-});
-
-//* SCROLL TO TOP END
-
-
-
-
-// const scrollToNext = document.querySelector(".scroll");
